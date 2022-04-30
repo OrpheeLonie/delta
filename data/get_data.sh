6e7fb4f1b52b1a368db3f8ca2e011cacbbc66824
Emmanuel <emmanuel.mollard@epita.fr>
2022-04-30 17:15:05 +0200

    [get_data] add a first version in shell (with wget)

---
+++ data/get_data.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+
+load_file () {
+    address="$1"
+    outputFile="$2"
+
+    if [ -f "$outputFile" ]
+    then
+        rm "$outputFile"
+    fi
+
+    wget "$address" --output-document="$outputFile"
+
+    return 0
+}
+
+load_file 'https://www.data.gouv.fr/fr/datasets/r/fdf5afbf-ed3c-4c54-a4f0-3581c8a1eca4' 'tableaux-4001-ts.xlsx'
+load_file 'https://www.data.gouv.fr/fr/datasets/r/65b3e15a-735c-4b68-aff3-cc25f74eb1c1' 'tableaux-4001-pn.xlsx'
+load_file 'https://www.data.gouv.fr/fr/datasets/r/2cfdabbb-bfbe-4e39-8f96-ccf7a6b0b572' 'tableaux-4001-gn.xlsx'
